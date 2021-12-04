886331fa7a727934593cbae3974aeaca6aefb25f
breadisstale <88885783+breadisstale@users.noreply.github.com>
2021-12-04 22:04:25 +0200

    V13

--- js/alivio/script.js
+++ js/alivio/script.js
@@ -1,6 +1,24 @@
 "use strict"
 window.addEventListener("DOMContentLoaded", () => {
 
+    function testWebP(callback) {
+
+        var webP = new Image();
+        webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        
+        testWebP(function (support) {
+        
+        if (support == true) {
+        document.querySelector('body').classList.add('webp');
+        }else{
+        document.querySelector('body').classList.add('no-webp');
+        }
+        });
+
     // Sidenav menu
     const sidenavIcon = document.querySelector('.sidenav-icon');
     const closeSidenav = document.querySelector('.sidenav .closebtn');
